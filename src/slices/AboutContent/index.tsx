f10af8bc790ee39417709e00597eb22480bba4b1
marius-jopen <marius.jopen@thepeople.world>
2024-07-04 10:59:02 +0200

    images included

--- src/slices/AboutContent/index.tsx
+++ src/slices/AboutContent/index.tsx
@@ -7,59 +7,54 @@ const AboutContent = ({ slice }: AboutContentProps): JSX.Element => {
   return (
     <section>
       <div className="grid grid-cols-12 grid-flow-row auto-rows-max md:grid-cols-24 distance-top-4 distance-bottom-5">
-        <div className="row-start-1 col-start-2 col-end-12 md:col-end-13 bg-white/30 text-style-7 text-text-gray-on-black">
+        <div className="row-start-1 col-start-2 col-end-12 md:col-end-13 text-style-7 text-text-gray-on-white">
           <PrismicRichText field={slice.primary.text_1} />
         </div>
 
-        <div className="row-start-2 col-start-1 md:col-start-2 col-end-12 bg-white/30">
+        <div className="row-start-2 col-start-1 md:col-start-2 col-end-12">
           <PrismicImage field={slice.primary.image_1} />
-          Image 1
 
-          <div className="text-text-gray-on-black text-style-10">
+          <div className="text-text-gray-on-white text-style-10">
             <PrismicRichText field={slice.primary.caption_1} />
           </div>
         </div>
 
-        <div className="row-start-1 col-start-2 md:col-start-15 col-end-13 md:col-end-22 bg-white/30">
+        <div className="row-start-1 col-start-2 md:col-start-15 col-end-13 md:col-end-22">
           <PrismicImage field={slice.primary.image_2} />
-          Image 2
 
-          <div className="text-text-gray-on-black text-style-10">
+          <div className="text-text-gray-on-white text-style-10">
             <PrismicRichText field={slice.primary.caption_2} />
           </div>
         </div>
 
-        <div className="row-start-3 col-start-2 col-end-12 md:col-end-20 bg-white/30 text-style-4 text-text-gray-on-black">
+        <div className="row-start-3 col-start-2 col-end-12 md:col-end-20 text-style-4 text-text-gray-on-white">
           <PrismicRichText field={slice.primary.text_2} />
         </div>
 
-        <div className="row-start-4 col-start-2 col-end-12 md:col-end-10 bg-white/30">
+        <div className="row-start-4 col-start-2 col-end-12 md:col-end-10">
           <PrismicImage field={slice.primary.image_3} />
-          Image 3
 
-          <div className="text-text-gray-on-black text-style-10">
+          <div className="text-text-gray-on-white text-style-10">
             <PrismicRichText field={slice.primary.caption_3} />
           </div>
         </div>
 
-        <div className="row-start-4 col-start-2 md:col-start-15 col-end-12 md:col-end-23 bg-white/30 text-style-7 text-text-gray-on-black">
+        <div className="row-start-4 col-start-2 md:col-start-15 col-end-12 md:col-end-23 text-style-7 text-text-gray-on-white">
           <PrismicRichText field={slice.primary.text_3} />
         </div>
 
-        <div className="row-start-5 col-start-1 md:col-start-12 col-end-12 md:col-end-23 bg-white/30">
+        <div className="row-start-5 col-start-1 md:col-start-12 col-end-12 md:col-end-23">
           <PrismicImage field={slice.primary.image_4} />
-          Image 4
 
-          <div className="text-text-gray-on-black text-style-10">
+          <div className="text-text-gray-on-white text-style-10">
             <PrismicRichText field={slice.primary.caption_4} />
           </div>
         </div>
 
-        <div className="md:mt-[-200px] row-start-6 col-start-2 col-end-13 md:col-end-10 bg-white/30">
+        <div className="row-start-6 col-start-2 col-end-13 md:col-end-10">
           <PrismicImage field={slice.primary.image_5} />
-          Image 5
 
-          <div className="text-text-gray-on-black text-style-10">
+          <div className="text-text-gray-on-white text-style-10">
             <PrismicRichText field={slice.primary.caption_5} />
           </div>
         </div>
--- src/slices/ImageOpener/index.tsx
+++ src/slices/ImageOpener/index.tsx
@@ -6,14 +6,13 @@ export type ImageOpenerProps = SliceComponentProps<Content.ImageOpenerSlice>;
 const ImageOpener = ({ slice }: ImageOpenerProps): JSX.Element => {
   return (
     <section className="bg-black text-white">
-      <div className="grid grid-cols-12 md:grid-cols-24 h-[70vw]">
-        <div className="col-start-2 col-end-12 md:col-end-24 text-style-9 text-white">
+      <div className="relative grid grid-cols-12 md:grid-cols-24 h-[50vh] md:h-[55vw]">
+        <div className="relative z-10 col-start-2 col-end-12 md:col-end-24 text-style-9 text-white distance-top-1">
           <PrismicRichText field={slice.primary.headline} />
         </div>
         
-        <div className="col-span-24">
-          <PrismicImage field={slice.primary.image} />
-          Image
+        <div className="absolute h-full w-full top-0 left-0">
+          <PrismicImage className="object-cover h-full" field={slice.primary.image} />
         </div>
       </div>
     </section>
