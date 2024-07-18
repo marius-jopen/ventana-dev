76d022461e69fa6605e5c416e21df9c1379872d1
marius-jopen <marius.jopen@thepeople.world>
2024-07-18 21:14:20 +0200

    styling

--- src/slices/AboutContent/index.tsx
+++ src/slices/AboutContent/index.tsx
@@ -11,52 +11,62 @@ const AboutContent = ({ slice }: AboutContentProps): JSX.Element => {
           <PrismicRichText field={slice.primary.text_1} />
         </div>
 
-        <div className="row-start-2 col-start-1 md:col-start-2 col-end-12">
+        <div className="row-start-2 md:row-start-2 col-start-1 md:col-start-2 col-end-12">
           <PrismicImage field={slice.primary.image_1} />
-
-          <div className="text-text-gray-on-white text-style-10">
-            <PrismicRichText field={slice.primary.caption_1} />
+          
+          {slice.primary.caption_1 && (
+            <div className="text-text-gray-on-white text-style-10">
+              <PrismicRichText field={slice.primary.caption_1} />
           </div>
+          )}
         </div>
 
-        <div className="row-start-1 col-start-2 md:col-start-15 col-end-13 md:col-end-22">
+        <div className="row-start-3 md:row-start-1 col-start-2 md:col-start-15 col-end-13 md:col-end-22">
           <PrismicImage field={slice.primary.image_2} />
 
-          <div className="text-text-gray-on-white text-style-10">
-            <PrismicRichText field={slice.primary.caption_2} />
-          </div>
+          {slice.primary.caption_2 && (
+            <div className="text-text-gray-on-white text-style-10 pt-2 pl-2">
+              <PrismicRichText field={slice.primary.caption_2} />
+            </div>
+          )}
         </div>
 
-        <div className="row-start-3 col-start-2 col-end-12 md:col-end-20 text-style-4 text-text-gray-on-white">
+        <div className="row-start-4 md:row-start-3 col-start-2 col-end-12 md:col-end-20 text-style-4 text-text-gray-on-white distance-top-1 distance-bottom-1">
           <PrismicRichText field={slice.primary.text_2} />
         </div>
 
-        <div className="row-start-4 col-start-2 col-end-12 md:col-end-10">
+        <div className="row-start-5 md:row-start-4 col-start-2 col-end-12 md:col-end-10">
           <PrismicImage field={slice.primary.image_3} />
 
-          <div className="text-text-gray-on-white text-style-10">
-            <PrismicRichText field={slice.primary.caption_3} />
-          </div>
+          {slice.primary.caption_3 && (
+            <div className="text-text-gray-on-white text-style-10 pt-2 pl-2">
+              <PrismicRichText field={slice.primary.caption_3} />
+            </div>
+          )}
         </div>
 
-        <div className="row-start-4 col-start-2 md:col-start-15 col-end-12 md:col-end-23 text-style-7 text-text-gray-on-white">
+        <div className="row-start-6 md:row-start-4 col-start-2 md:col-start-15 col-end-12 md:col-end-23 text-style-7 text-text-gray-on-white">
           <PrismicRichText field={slice.primary.text_3} />
         </div>
 
-        <div className="row-start-5 col-start-1 md:col-start-12 col-end-12 md:col-end-23">
+        <div className="row-start-7 md:row-start-5 col-start-1 md:col-start-12 col-end-12 md:col-end-23">
           <PrismicImage field={slice.primary.image_4} />
 
-          <div className="text-text-gray-on-white text-style-10">
-            <PrismicRichText field={slice.primary.caption_4} />
-          </div>
+          {slice.primary.caption_4 && (
+            <div className="text-text-gray-on-white text-style-10 pt-2 pl-2">
+              <PrismicRichText field={slice.primary.caption_4} />
+            </div>
+          )}
         </div>
 
-        <div className="row-start-6 col-start-2 col-end-13 md:col-end-10">
+        <div className="row-start-8 md:row-start-6 col-start-2 col-end-13 md:col-end-10">
           <PrismicImage field={slice.primary.image_5} />
 
-          <div className="text-text-gray-on-white text-style-10">
-            <PrismicRichText field={slice.primary.caption_5} />
-          </div>
+          {slice.primary.caption_5 && (
+            <div className="text-text-gray-on-white text-style-10 pt-2 pl-2">
+              <PrismicRichText field={slice.primary.caption_5} />
+            </div>
+          )}
         </div>
       </div>
     </section>
