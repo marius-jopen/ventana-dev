06fb33e472c7efc3683f9e4f2ef908f6d8dce0f4
marius-jopen <marius.jopen@thepeople.world>
2024-08-01 09:58:54 +0200

    home done

--- src/slices/DoubleText/index.tsx
+++ src/slices/DoubleText/index.tsx
@@ -19,16 +19,16 @@ const DoubleText = ({ slice }: DoubleTextProps): JSX.Element => {
 
   return (
     <section 
-    className="h-full block bg-white mt-[-1px]"
+    className="h-full block bg-white mt-[-1px] overflow-hidden"
     slice-name="double-text"
     data-slice="style-white"
     >
-      <div className="grid grid-cols-12 md:grid-cols-24 grid-flow-row auto-rows-max distance-top-3 distance-bottom-5">
+      <div className="grid grid-cols-12 md:grid-cols-24 grid-flow-row auto-rows-max pt-12 md:pt-20 2xl:pt-28 pb-28 md:pb-32 2xl:pb-48">
         <div data-aos="fade-up" className="pb-12 md:pb-0 row-start-1 col-start-2 col-end-12 bg-white/30 text-style-11 text-black">
           <PrismicRichText field={slice.primary.headline} />
         </div>
         
-        <div data-aos="fade-up" className="pb-24 md:pb-0 md:mt-1 row-start-2 md:row-start-1 col-start-2 md:col-start-13 col-end-12 md:col-end-24 bg-white/30 text-style-6 text-text-gray-on-white ">
+        <div data-aos="fade-up" className="md:mt-1 row-start-2 md:row-start-1 col-start-2 md:col-start-13 col-end-12 md:col-end-24 bg-white/30 text-style-6 text-text-gray-on-white ">
           <PrismicRichText field={slice.primary.text} />
         </div>
       </div>
